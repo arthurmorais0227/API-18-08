739eda3962376a0d610879bc70bf20256623b2ca
Arthur <mlkliso4000@gmail.com>
2025-08-18 09:25:59 -0300

    5 novos bruxos

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
-import express from 'express';
-import bruxos from './src/data/bruxos.js';
+import express from "express";
+import bruxos from "./src/data/bruxos.js";
 
 const app = express();
 const serverPort = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1> Minha API de Harry Potter está ativa! <h1/>")
+  res.send("<h1> Minha API de Harry Potter está ativa! <h1/>");
 });
 
 app.get("/bruxos", (req, res) => {
-    res.json(bruxos);
+  res.json(bruxos);
 });
 
 app.get("/bruxos/:id", (req, res) => {
-    let id = req.params.id;
-    id = parseInt(id);
-    console.log(typeof(id));
-    const bruxo = bruxos.find(b => b.id === id);
-    console.log(bruxo);
+  let id = req.params.id;
+  id = parseInt(id);
+  console.log(typeof id);
+  const bruxo = bruxos.find((b) => b.id === id);
+  console.log(bruxo);
 
-    if(bruxo) {
-        res.status(200).json(bruxo);
-    } else {
-        res.status(400).json
-        mensagem: "Bruxo não encontrado!"
-    }
-})
+  if (bruxo) {
+    res.status(200).json(bruxo);
+  } else {
+    res.status(404).json({
+      mensagem: "Bruxo não encontrado!",
+    });
+  }
+});
 
 app.listen(3000, () => {
-    console.log(`O servidor está funcionando no link http://localhost: ${serverPort}`)
+  console.log(
+    `O servidor está funcionando no link http://localhost: ${serverPort}`
+  );
 });
